9475322d2709b6e626d40a264d78a00b88272267
Kirill Shilov <tvix2000@yandex.ru>
2015-11-18 16:34:54 +0400

    Update AkFinance-js.js
    some methods

--- pages/jQuery/AkFinance-js.js
+++ pages/jQuery/AkFinance-js.js
@@ -1,6 +1,22 @@
+/* First method */
 $(document).ready(function(){
     var helperLink = $('#helperLink');
     var helperBlock = $('.helper-pass').addClass('hide');
+
+        helperLink.on('mouseover', function(){
+            helperBlock.removeClass('hide').addClass('show');
+        });
+        helperLink.on('mouseleave', function(){
+           helperBlock.removeClass('show').addClass('hide');
+        });
+});
+
+/* Second method */
+$(document).ready(function(){
+    var helperLink = $('a');
+        helperLink.find('#helperLink');
+    var helperBlock = $('.helper-pass')
+                     .addClass('hide');
 
         helperLink.on('mouseover', function(){
             helperBlock.removeClass('hide').addClass('show');
